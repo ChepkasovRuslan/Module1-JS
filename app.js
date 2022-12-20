6a1e64561d2d17ff868ecb2e5066269944eec255
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-20 14:07:23 +0300

    task22

---
+++ app.js
@@ -0,0 +1,7 @@
+const numArray = [13, 14, 32, 23, 34, 21, 44, 47, 86];
+const start = 4;
+const end = 6;
+
+const cut = (array, start, end) => Array.from(array).slice(start, end + 1); 
+
+console.log(cut(numArray, start, end));
