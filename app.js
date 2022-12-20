d5e7f73b6e2c7873ff201989c7bb01f21a3fb7ff
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-20 14:10:36 +0300

    task29

---
+++ app.js
@@ -0,0 +1,15 @@
+const obj = {
+    id: 123,
+    first_name: 'Ivan',
+    last_name: 'Ivanov',
+    password: 'new password',
+    age: 15
+}
+
+const { id, first_name, last_name, password, age } = obj;
+
+console.log(id);
+console.log(first_name);
+console.log(last_name);
+console.log(password);
+console.log(age);
