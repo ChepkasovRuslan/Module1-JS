d0e2ccf546d4fca4deb792a5c25a5e1722fa1653
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-22 17:30:24 +0300

    task 10 fix

--- task10.js
+++ task10.js
@@ -1,14 +1,14 @@
 const payments = [
-    { from: 'Ivan', to: 'Oleg', amount: 2500}, 
-    { from: 'Ivan', to: 'Igor', amount: 2000}, 
-    { from: 'Oleg', to: 'Igor', amount: 1500}
+  { from: 'Ivan', to: 'Oleg', amount: 2500 }, 
+  { from: 'Ivan', to: 'Igor', amount: 2000 }, 
+  { from: 'Oleg', to: 'Igor', amount: 1500 }
 ];
 
 const average =
-    payments.reduce((curr, next) => curr + next.amount, 0) / payments.length;
+  payments.reduce((previousValue, currentValue) => previousValue + currentValue.amount, 0) / payments.length;
 
 payments.forEach(element => {
-    element.diff = element.amount - average;
+  element.diff = element.amount - average;
 });
 
 console.log(average);
