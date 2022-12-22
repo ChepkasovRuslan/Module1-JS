a38ce732ad1e0edb6290c28d102e200a338b6416
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-22 17:53:18 +0300

    fixed task13

--- task13.js
+++ task13.js
@@ -1,14 +1,25 @@
 const value =  ['str', 123, '123', true, null, {}, 2];
 
 const determineType = value => {
-    let result;
+    let result = '';
     switch(typeof(value)) {
-        case 'string': result = value + ' - строка'; break;
-        case 'number': result = value + ' - число'; break;
-        case 'boolean': result = value + ' - булевое значение'; break;
-        default: result = value + ' - неизвестное значение';
+        case 'string': {
+            result = value + ' - строка'; 
+            break;
+        }
+        case 'number': {
+            result = value + ' - число';
+            break;
+        }
+        case 'boolean':{
+            result = value + ' - булевое значение';
+            break;
+        } 
+        default: {
+            result = value + ' - неизвестное значение';
+        } 
     }
     return result;
 }
 
-let printTypes = value.forEach(i => console.log(determineType(i)));
+const printTypes = value.forEach(i => console.log(determineType(i)));
