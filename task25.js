03e6041a254a21593939d94d6c9022f06f6675e7
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-23 09:48:50 +0300

    fix task25

--- task25.js
+++ task25.js
@@ -1,7 +1,11 @@
 const snakeString = 'find_and_replace_element_of_array';
 
-const toCamelCase = str => str.split('_')
-        .map(i => i[0].toUpperCase() + i.substring(1))
-        .join('');
+const toCamelCase = str => {
+  let result = str.split('_')
+    .map(i => i[0].toUpperCase() + i.substring(1))
+    .join('')
+
+  return result[0].toLowerCase() + result.substring(1, result.length);
+}
 
 console.log(toCamelCase(snakeString));
