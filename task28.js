cc28d85a0f2750c968f6b4b354122990ca3d289b
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-23 09:55:32 +0300

    fix task28

--- task28.js
+++ task28.js
@@ -12,14 +12,12 @@ const changed = {
 };
 
 const updateUser = (user, changedUser) => {
-    let result = user;
-    if(changedUser.id) result.id = changedUser.id;
-    if(changedUser.first_name) result.first_name = changedUser.first_name;
-    if(changedUser.last_name) result.last_name = changedUser.last_name;
-    if(changedUser.password) result.password = changedUser.password;
-    if(changedUser.age) result.age = changedUser.age;
-
-    return result;
+    return {
+        newUser: {
+            ...user,
+            ...changedUser
+        }
+    }
 }
 
 console.log(updateUser(user, changed));
