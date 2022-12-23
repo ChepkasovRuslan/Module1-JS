7cf6f88d433aad3c74b73bdaa73e45877c8f0a8a
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-23 12:48:56 +0300

    fixed 25

--- task25.js
+++ task25.js
@@ -1,24 +1,19 @@
 const snakeString = 'find_and_replace_element_of_array';
 
 const toCamelCase = str => {
-  let result = str;
+  let result = '';
 
-  for (let i = 1; i < result.length; i++) {
-    if(result[i] === '_')  {
-      result = removeChar(result, i)
-      result = result.substring(0, i) + result[i].toUpperCase() + result.substring(i + 1);
+  for (let i = 0; i < str.length; i++) {
+    if(str[i] === '_')  {
+      result += str[i + 1].toUpperCase();
+      i++;
+    }
+    else {
+      result += str[i];
     }
   }
 
   return result;
-}
-
-function removeChar(str, char_pos) 
-{
-  part1 = str.substring(0, char_pos);
-  part2 = str.substring(char_pos + 1, str.length);
-  
-  return part1 + part2;
 }
 
 console.log(toCamelCase(snakeString));
