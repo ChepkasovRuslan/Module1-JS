674e314018699da7be4f5f6a9465ece82255eb67
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-23 10:49:01 +0300

    without reduce

--- task9.js
+++ task9.js
@@ -4,12 +4,26 @@ const persons = [
     { name: 'Igor', age: 24}
 ];
 
-const highestAge = persons.reduce((previous, current) => {
-    return current.age > previous.age ? current : previous;
-}).age;
+const highestAge = persons => {
+    let max = 0;
+    for (let i = 0; i < persons.length; i++) {
+        if(persons[i].age > max) {
+            max = persons[i].age;
+        }
+    }
 
-const lowestAge = persons.reduce((previous, current) => {
-    return current.age < previous.age ? current : previous;
-}).age;
+    return max;
+}
 
-console.log({ min: lowestAge, max: highestAge });
+const lowestAge = persons => {
+    let min = Number.MAX_VALUE;
+    for (let i = 0; i < persons.length; i++) {
+        if(persons[i].age < min) {
+            min = persons[i].age;
+        }
+    }
+
+    return min;
+}
+
+console.log({ min: lowestAge(persons), max: highestAge(persons) });
