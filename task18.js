5ff12dfd1932d140304e8780d76465c4e5bc5ab3
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-23 13:34:44 +0300

    without split

--- task18.js
+++ task18.js
@@ -1,6 +1,21 @@
 const fullname = 'Иванов Пётр Андреевич';
 
-const names = fullname.split(' ');
+const splitNames = fullname => {
+    const names = [''];
+    var j = 0;
+
+    for (var i = 0; i < fullname.length; i++) {
+        if (fullname.charAt(i) === ' ') {
+            j++;
+            names.push('');
+        } else {
+            names[j] += fullname.charAt(i);
+        }
+    }
+    return names;
+}
+
+const names = splitNames(fullname);
 
 const fullnameObj = {
     first_name: names[1],
