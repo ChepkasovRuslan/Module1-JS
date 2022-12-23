89aaa0c2e35b9e36b204e635f0bc16a309b9593a
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-23 10:25:07 +0300

    fix task20

--- task20.js
+++ task20.js
@@ -1,9 +1,16 @@
 const url = new URL('https://www.google.com/doodles/rubiks-cube');
 
+const urlPath = url.pathname.substring(1).split('/');
+
+console.log(urlPath);
+
 const urlObject = {
     protocol: url.protocol,
     hostname: url.hostname,
-    path: url.pathname
+}
+
+for (let i = 0; i < urlPath.length; i++) {
+    urlObject.child = ({path: urlPath[i]});
 }
 
 console.log(urlObject);
