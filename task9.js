73b155e19b021128a2d3b6fe68a2cf7bbe108b0b
Ruslan Chepkasov <chepkasov.itrum@gmail.com>
2022-12-26 12:43:07 +0300

    one cycle

--- task9.js
+++ task9.js
@@ -4,26 +4,21 @@ const persons = [
     { name: 'Igor', age: 24}
 ];
 
-const highestAge = persons => {
+const findMinMax = persons => {
     let max = 0;
+    let min = Number.MAX_VALUE;
     for (let i = 0; i < persons.length; i++) {
         if(persons[i].age > max) {
             max = persons[i].age;
         }
-    }
-
-    return max;
-}
-
-const lowestAge = persons => {
-    let min = Number.MAX_VALUE;
-    for (let i = 0; i < persons.length; i++) {
         if(persons[i].age < min) {
             min = persons[i].age;
         }
     }
 
-    return min;
+    return {max: max, min: min};
 }
 
-console.log({ min: lowestAge(persons), max: highestAge(persons) });
+const result = findMinMax(persons);
+
+console.log(result);
